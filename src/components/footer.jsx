c83084a6f6aa24445206cdc441b74d51b3d05cea
priyankarpal <88102392+priyankarpal@users.noreply.github.com>
2023-04-09 13:54:20 +0530

    fix: footer problem solved #4

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -2,8 +2,8 @@ import React from "react";
 
 function Footer() {
   return (
-    <footer className="flex flex-wrap p-5  text-black">
-      <p className="w-1/2 mx-auto">Made with ❤️ by html dev</p>
+    <footer className="flex justify-center flex-wrap p-4  text-black">
+      <p className=" lg:w-1/2 mx-auto">Made with ❤️ by html dev</p>
     </footer>
   );
 }
