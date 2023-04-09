021e3433f8fe2705d8ea170076308f1236425ba8
priyankarpal <88102392+priyankarpal@users.noreply.github.com>
2023-04-09 13:58:08 +0530

    fix: navbar position #2

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -3,8 +3,8 @@ import React from "react";
 function Header() {
   return (
     <header>
-      <nav className=" flex h-[5rem] pt-5 w-full">
-        <p className="w-1/2 mx-auto font-semibold text-xl">
+      <nav className=" flex justify-center h-[5rem] pt-5 w-full">
+        <p className="lg:w-1/2 mx-auto font-semibold text-xl">
           priyank<span className="text-yellow-500">.js</span>
         </p>
       </nav>
