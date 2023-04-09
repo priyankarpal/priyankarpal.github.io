e19950703ab2afa6442870531db8426782ddbbd6
priyankarpal <88102392+priyankarpal@users.noreply.github.com>
2023-04-09 15:13:08 +0530

    chore: added footer link

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -3,7 +3,12 @@ import React from "react";
 function Footer() {
   return (
     <footer className="flex justify-center flex-wrap p-4  text-black">
-      <p className=" lg:w-1/2 mx-auto">Made with ❤️ by html dev</p>
+      <p className=" lg:w-1/2 mx-auto">
+        Made with ❤️ by{" "}
+        <a href="https://twitter.com/priyankarpal" target="_blank">
+          html dev
+        </a>
+      </p>
     </footer>
   );
 }
