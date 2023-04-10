fb1341ac27ce4f278e61c4c674ab11206e86f752
priyankarpal <88102392+priyankarpal@users.noreply.github.com>
2023-04-10 09:57:38 +0530

    feat: added vercel analytics code #9

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
+import { inject } from "@vercel/analytics";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+inject();
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
