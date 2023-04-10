56066bcf41480bb45b5f8f3f3639572aac9ba19e
priyankarpal <88102392+priyankarpal@users.noreply.github.com>
2023-04-10 12:37:43 +0530

    chore: added skills image to center for mobile view

--- src/components/skills.jsx
+++ src/components/skills.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function Skills({ image, name }) {
   return (
     <section>
-      <div className="max-w-sm p-6 ">
+      <div className="lg:max-w-sm p-6  flex justify-center ">
         <img src={image} alt={name} className="h-10" />
       </div>
     </section>
