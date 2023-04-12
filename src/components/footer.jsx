6ac2fed227e75b3c7d86ed24ba6b92baa06eac86
Jay Kumar Gupta <100681165+jayk-gupta@users.noreply.github.com>
2023-04-12 22:03:45 +0530

    added contribution link

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,14 +1,24 @@
 import React from "react";
+import {BiGitRepoForked} from "react-icons/bi";
 
 function Footer() {
   return (
-    <footer className="flex justify-center flex-wrap p-4  text-black">
-      <p className=" lg:w-1/2 mx-auto">
-        Made with ❤️ by {/* Throw some respect on my name, homie! 😅 😂  */}
-        <a href="https://twitter.com/priyankarpal" target="_blank">
-          html dev
-        </a>
-      </p>
+    <footer className="flex  p-4  text-black">
+      <div className=" lg:w-1/2 mx-auto flex justify-between">
+        <span>
+          Made with ❤️ by {/* Throw some respect on my name, homie! 😅 😂  */}
+          <a href="https://twitter.com/priyankarpal" target="_blank">
+            html dev
+          </a>
+        </span>
+          <a
+            className="flex flex-row items-center space-x-2"
+            href="https://github.com/priyankarpal/Portfolio"
+            target="_blank"
+          >
+            <p> Contribute here</p> <BiGitRepoForked />
+          </a>
+      </div>
     </footer>
   );
 }
