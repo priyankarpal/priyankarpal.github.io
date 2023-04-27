32266ba5dfa9eb07708020fe15e755ef5bc3db6c
priyankarpal <88102392+priyankarpal@users.noreply.github.com>
2023-04-27 15:48:52 +0530

    chore: fork icon increased

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import {BiGitRepoForked} from "react-icons/bi";
+import { BiGitRepoForked } from "react-icons/bi";
 
 function Footer() {
   return (
@@ -7,17 +7,17 @@ function Footer() {
       <div className=" lg:w-1/2 mx-auto flex justify-between text-sm">
         <span className="mr-8">
           Made with ❤️ by {/* Throw some respect on my name, homie! 😅 😂  */}
-          <a href="https://twitter.com/priyankarpal" target="_blank">
+          <a href="https://twitter.com/priyankarpal" target="_blank" className="text-purple-400">
             html dev
           </a>
         </span>
-          <a
-            className="flex flex-row items-center space-x-2"
-            href="https://github.com/priyankarpal/Portfolio"
-            target="_blank"
-          >
-            <p className="hidden lg:visible"> Contribute here</p> <BiGitRepoForked  />
-          </a>
+        <a
+          className="flex flex-row items-center space-x-2"
+          href="https://github.com/priyankarpal/Portfolio"
+          target="_blank"
+        >
+          <BiGitRepoForked className="text-[1.3rem]" />
+        </a>
       </div>
     </footer>
   );
