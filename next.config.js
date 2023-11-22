404740bfc898137a3acda3ab9b960adee02fc764
priyankarpal <88102392+priyankarpal@users.noreply.github.com>
2023-11-22 14:19:55 +0530

    chore: remove image domains

--- next.config.js
+++ next.config.js
@@ -1,9 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    reactStrictMode: true,
-    images: {
-        domains: ['images.unsplash.com', 'github.com'],
-    }
-}
+const nextConfig = {}
 
 module.exports = nextConfig
